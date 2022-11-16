7f81d906216e06138ece052c1a7d3f9ac1cd8ce4
mohamed-amine-aouadi <48270211+mohamed-amine-aouadi@users.noreply.github.com>
2022-11-16 19:08:36 +0100

    Set JUnit Stock test

---
+++ src/test/java/tn/esprit/rh/achat/StockServiceImplMockTest.java
@@ -0,0 +1,48 @@
+package tn.esprit.rh.achat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.StockServiceImpl;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class StockServiceImplMockTest {
+    @InjectMocks
+    private StockServiceImpl stockServiceImpl;
+    @Mock
+    private StockRepository stockRepository;
+    @Test
+    public void retrieveAllStocks() {
+        when(stockRepository.findAll()).thenReturn(Stream.of(new Stock(),new Stock(),new Stock()).collect(Collectors.toList())) ;
+        assertEquals(3,stockServiceImpl.retrieveAllStocks().size());
+    }
+
+    @Test
+    public void addStock() {
+        Stock stock=new Stock();
+        when(stockRepository.save(stock)).thenReturn(stock);
+        assertEquals(stock, stockServiceImpl.addStock(stock));
+    }
+
+    @Test
+    public void updateStock() {
+        Stock stock=new Stock();
+        when(stockRepository.save(stock)).thenReturn(stock);
+        assertEquals(stock, stockServiceImpl.updateStock(stock));
+    }
+
+    @Test
+    public void retrieveStock(){
+        Stock stock = new Stock();
+        when(stockRepository.findById(stock.getIdStock())).thenReturn(java.util.Optional.of(stock));
+        assertEquals(stock.getIdStock(),stockServiceImpl.retrieveStock(stock.getIdStock()).getIdStock());
+    }
+}
--- src/test/java/tn/esprit/rh/achat/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/StockServiceImplTest.java
@@ -1,48 +1,49 @@
 package tn.esprit.rh.achat;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.StockServiceImpl;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.List;
+
+@SpringBootTest
 public class StockServiceImplTest {
-    @InjectMocks
+    @Autowired
     private StockServiceImpl stockServiceImpl;
-    @Mock
+    @Autowired
     private StockRepository stockRepository;
     @Test
     public void retrieveAllStocks() {
-        when(stockRepository.findAll()).thenReturn(Stream.of(new Stock(),new Stock(),new Stock()).collect(Collectors.toList())) ;
-        assertEquals(3,stockServiceImpl.retrieveAllStocks().size());
+       List<Stock> stockList = stockServiceImpl.retrieveAllStocks();
+        Assertions.assertEquals(0,stockList.size());
     }
 
     @Test
     public void addStock() {
         Stock stock=new Stock();
-        when(stockRepository.save(stock)).thenReturn(stock);
-        assertEquals(stock, stockServiceImpl.addStock(stock));
+        stock.setLibelleStock("ADDStock");
+        stock.setQte(150);
+        stock.setQteMin(50);
+        Stock s = stockServiceImpl.addStock(stock);
+        Assertions.assertEquals(stock.getLibelleStock(), s.getLibelleStock());
     }
 
     @Test
     public void updateStock() {
-        Stock stock=new Stock();
-        when(stockRepository.save(stock)).thenReturn(stock);
-        assertEquals(stock, stockServiceImpl.updateStock(stock));
+
+        Stock stock=stockServiceImpl.retrieveStock(1L);
+        stock.setLibelleStock("UPDATEStock");
+        Stock s = stockServiceImpl.updateStock(stock);
+        Assertions.assertEquals(stock.getLibelleStock(), s.getLibelleStock());
     }
 
     @Test
     public void retrieveStock(){
-        Stock stock = new Stock();
-        when(stockRepository.findById(stock.getIdStock())).thenReturn(java.util.Optional.of(stock));
-        assertEquals(stock.getIdStock(),stockServiceImpl.retrieveStock(stock.getIdStock()).getIdStock());
+        Stock stock=stockServiceImpl.retrieveStock(1L);
+        Assertions.assertEquals(1L,stock.getIdStock());
     }
 }
